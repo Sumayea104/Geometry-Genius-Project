45a2ed302f83ac4d8181cc26bbb651b0a8bffd0c
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-21 14:36:42 +0600

    first function worked

--- js/final.js
+++ js/final.js
@@ -1,19 +1,7 @@
-// // console.log('okay');
-// // first card
 // document.getElementById('first-card').addEventListener('click', function () {
-//     // console.log('first btn clicked');
-//     // console.log(e.target);-----function(e)
-//     // get the data from html using id
 //     const FirstElement = document.getElementById('First-name').innerText;
-//     // console.log(FirstElement);
 //     const base = document.getElementById('triangle-base').value;
-//     // console.log(base);
 //     const height = document.getElementById('triangle-height').value;
-//     // console.log(height);
-//     // console.log(FirstElement,base,height);
-    
-
-//     // Calculate the area of the triangle
 //     const area = 0.5 * base * height;
 
 //     const container = document.getElementById('table-container');
@@ -27,4 +15,5 @@
 
 //     `;
 //     container.appendChild(tr);
+
 // });

db6a5d2d1155dc5b547b3e9329d69d402b100880
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-21 18:48:03 +0600

    second card

--- js/test.js
+++ js/test.js
@@ -1,22 +1,37 @@
+// first card
 document.getElementById('first-card').addEventListener('click', function () {
-    const firstElement = document.getElementById('First-name').innerText;
+    const cardElement = document.getElementById('First-name').innerText;
     const base = document.getElementById('triangle-base').value;
     const height = document.getElementById('triangle-height').value;
     const area = 0.5 * base * height;
-    
-    
-    displayData(firstElement,area);
+
+
+    displayData(cardElement, area);
+});
+// second card
+document.getElementById('second-card').addEventListener('click', function () {
+    const cardElement = document.getElementById('second-title').innerText;
+    const Width = document.getElementById('rectangle-width').value;
+    const length = document.getElementById('rectangle-length').value;
+    const area = Width * length;
+
+    displayData(cardElement, area);
 });
 
-function displayData(firstElement,area){
+
+
+
+
+// same work
+function displayData(cardElement, area) {
     const container = document.getElementById('table-container');
     let count = container.getElementsByTagName('tr').length;
     count += 1;
     const tr = document.createElement('tr');
     tr.innerHTML = `
     <td>${count}</td>
-    <td>${firstElement}</td>
+    <td>${cardElement}</td>
     <td>${area}</td>
     `;
     container.appendChild(tr);
-    }
+}

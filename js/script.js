94323cdf4626acf33e7039f65dc00adbe6e82756
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-21 12:04:16 +0600

    area js

--- js/script.js
+++ js/script.js
@@ -25,3 +25,31 @@ function calculateParallelogramArea() {
     const area = b * h;
     alert(`The area of the parallelogram is ${area} square cm.`);
 }
+// reusable function for calculating rhombus area
+function calculateRhombusArea() {
+    const d1 = document.getElementById("rhombus-d1").innerText;
+    const d2 = document.getElementById("rhombus-d2").innerText;
+    const area = 0.5 * d1 * d2;
+    alert(`The area of the rhombus is ${area} square cm.`);
+}
+// add event listeners to the "Calculate" buttons
+document.querySelector(".calculate-parallelogram-area").addEventListener("click", calculateParallelogramArea);
+document.querySelector(".calculate-rhombus-area").addEventListener("click", calculateRhombusArea);
+
+
+// reusable function for calculating pentagon area
+function calculatePentagonArea() {
+    const p = document.getElementById("pentagon-p").innerText;
+    const b = document.getElementById("pentagon-b").innerText;
+    const area = 0.5 * p * b;
+    alert(`The area of the pentagon is ${area} square cm.`);
+}
+
+// reusable function for calculating ellipse area
+function calculateEllipseArea() {
+    const a = document.getElementById("ellipse-a").innerText;
+    const b = document.getElementById("ellipse-b").innerText;
+    const area = Math.PI * a * b;
+    const roundedArea = area.toFixed(2);
+    document.getElementById("ellipse-area").innerHTML = `<p>The area of the ellipse is ${roundedArea} square cm.</p>`;
+}

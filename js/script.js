7a365279da2a0d199ef2cf2f4dc1675722559c27
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-21 12:00:17 +0600

    rhombus

--- js/script.js
+++ js/script.js
@@ -16,3 +16,12 @@ calculateArea('.card.bg-indigo-200', (base, height) => 0.5 * base * height);
 
 // Card 02 - Rectangle
 calculateArea('.card.bg-violet-400', (width, length) => width * length);
+
+
+// reusable function for calculating parallelogram area
+function calculateParallelogramArea() {
+    const b = document.getElementById("parallelogram-b").innerText;
+    const h = document.getElementById("parallelogram-h").innerText;
+    const area = b * h;
+    alert(`The area of the parallelogram is ${area} square cm.`);
+}

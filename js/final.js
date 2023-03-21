de49266bbfb28c0d4abd67f4d7156900afd9c043
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-21 16:48:15 +0600

    processing

--- js/final.js
+++ js/final.js
@@ -1,5 +1,5 @@
 // document.getElementById('first-card').addEventListener('click', function () {
-//     const FirstElement = document.getElementById('First-name').innerText;
+//     const firstElement = document.getElementById('First-name').innerText;
 //     const base = document.getElementById('triangle-base').value;
 //     const height = document.getElementById('triangle-height').value;
 //     const area = 0.5 * base * height;
@@ -10,10 +10,34 @@
 //     const tr = document.createElement('tr');
 //     tr.innerHTML = `
 //     <td>${count}</td>
-//     <td>${FirstElement}</td>
+//     <td>${firstElement}</td>
 //     <td>${area}</td>
 
 //     `;
 //     container.appendChild(tr);
-
 // });
+
+
+
+// =-------------------------------------------
+
+
+// function calculateTriangleArea() {
+//     const FirstElement = document.getElementById('First-name').innerText;
+//     const base = document.getElementById('triangle-base').value;
+//     const height = document.getElementById('triangle-height').value;
+//     const area = 0.5 * base * height;
+
+//     const container = document.getElementById('table-container');
+//     let count = container.getElementsByTagName('tr').length;
+//     count += 1;
+//     const tr = document.createElement('tr');
+//     tr.innerHTML = `
+//         <td>${count}</td>
+//         <td>${FirstElement}</td>
+//         <td>${area}</td>
+//     `;
+//     container.appendChild(tr);
+// }
+
+// document.getElementById('first-card').addEventListener('click', calculateTriangleArea);
--- js/test.js
+++ js/test.js
@@ -1,30 +1,22 @@
-// // console.log('okay');
-// // first card
-// document.getElementById('first-card').addEventListener('click', function () {
-//     // console.log('first btn clicked');
-//     // console.log(e.target);-----function(e)
-//     // get the data from html using id
-//     const FirstElement = document.getElementById('First-name').innerText;
-//     // console.log(FirstElement);
-//     const base = document.getElementById('triangle-base').value;
-//     // console.log(base);
-//     const height = document.getElementById('triangle-height').value;
-//     // console.log(height);
-//     // console.log(FirstElement,base,height);
+document.getElementById('first-card').addEventListener('click', function () {
+    const firstElement = document.getElementById('First-name').innerText;
+    const base = document.getElementById('triangle-base').value;
+    const height = document.getElementById('triangle-height').value;
+    const area = 0.5 * base * height;
     
+    
+    displayData(firstElement,area);
+});
 
-//     // Calculate the area of the triangle
-//     const area = 0.5 * base * height;
-
-//     const container = document.getElementById('table-container');
-//     let count = 0;
-//     count += 1;
-//     const tr = document.createElement('tr');
-//     tr.innerHTML = `
-//     <td>${count}</td>
-//     <td>${FirstElement}</td>
-//     <td>${area}</td>
-
-//     `;
-//     container.appendChild(tr);
-// });
+function displayData(firstElement,area){
+    const container = document.getElementById('table-container');
+    let count = container.getElementsByTagName('tr').length;
+    count += 1;
+    const tr = document.createElement('tr');
+    tr.innerHTML = `
+    <td>${count}</td>
+    <td>${firstElement}</td>
+    <td>${area}</td>
+    `;
+    container.appendChild(tr);
+    }

cd9c18a4c2c56fea5c0629b26a0ff377309f3f93
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-21 19:08:52 +0600

    js calculation

--- js/test.js
+++ js/test.js
@@ -17,6 +17,42 @@ document.getElementById('second-card').addEventListener('click', function () {
 
     displayData(cardElement, area);
 });
+// third card
+document.getElementById('third-card').addEventListener('click', function () {
+    const cardElement = document.getElementById('third-title').innerText;
+    const base = document.getElementById('parallelogram-b').innerText;
+    const height = document.getElementById('parallelogram-h').innerText;
+    const area = parseInt(base) * parseInt(height);
+
+    displayData(cardElement, area);
+});
+// 4th card
+document.getElementById('fourth-card').addEventListener('click', function () {
+    const cardElement = document.getElementById('fourth-title').innerText;
+    const base = document.getElementById('rhombus-d1').innerText;
+    const height = document.getElementById('rhombus-d2').innerText;
+    const area = parseInt(base) * parseInt(height);
+
+    displayData(cardElement, area);
+});
+// fifth
+document.getElementById('fifth-card').addEventListener('click', function () {
+    const cardElement = document.getElementById('fifth-title').innerText;
+    const p = document.getElementById('pentagon-p').innerText;
+    const b = document.getElementById('pentagon-b').innerText;
+    const area =0.5 * parseInt(p) * parseInt(b);
+
+    displayData(cardElement, area);
+});
+// 6th
+document.getElementById('sixth-card').addEventListener('click', function () {
+    const cardElement = document.getElementById('sixth-title').innerText;
+    const a = document.getElementById('ellipse-a').innerText;
+    const b = document.getElementById('ellipse-b').innerText;
+    const area = parseInt(a) * parseInt(b);
+
+    displayData(cardElement, area);
+});
 
 
 

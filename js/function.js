360aa014044b6a50f2800ada3f834ee778de5193
Sumayea Rahman <shorbonam104@gmail.com>
2023-03-22 20:17:28 +0600

    updated

--- js/function.js
+++ js/function.js
@@ -1,19 +1,3 @@
-// function calculateTriangleArea() {
-//     const FirstElement = document.getElementById('First-name').innerText;
-//     const base = document.getElementById('triangle-base').value;
-//     const height = document.getElementById('triangle-height').value;
-//     const area = 0.5 * base * height;
-
-//     const container = document.getElementById('table-container');
-//     let count = container.getElementsByTagName('tr').length;
-//     count += 1;
-//     const tr = document.createElement('tr');
-//     tr.innerHTML = `
-//         <td>${count}</td>
-//         <td>${FirstElement}</td>
-//         <td>${area}</td>
-//     `;
-//     container.appendChild(tr);
-// }
-
-// document.getElementById('first-card').addEventListener('click', calculateTriangleArea);
+document.getElementById('edit-icon').addEventListener('click',function(){
+    
+})
